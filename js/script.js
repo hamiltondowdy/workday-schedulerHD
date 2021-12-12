bd4093f84e77e0d2c15da7fdbba0f141c2aa00c0
hamiltondowdy <92259496+hamiltondowdy@users.noreply.github.com>
2021-12-12 12:44:21 -0600

    added timeblocks and text area to html

--- js/script.js
+++ js/script.js
@@ -1,13 +1,18 @@
-var currentDate = document.querySelector("#currentDay")
+var currentDate = document.querySelector("#currentDay");
+var workDay = document.querySelector("#container");
 
+currentDate = moment();
+displayCurrentDate(currentDate);
 
-window.onload = function() {
-    const currentDate = moment();
-    
-    displayCurrentDate(currentDate);
-}
+workDay = container();
+displayWorkDay(workDay);
+
 
 function displayCurrentDate(currentDate) {
     document.getElementById("currentDay")
     .textContent=currentDate.format("MM/DD/YYYY");
+}
+
+function displayWorkDay(workDay) {
+    document.getElementById("container").appendChild(workDay);
 }
