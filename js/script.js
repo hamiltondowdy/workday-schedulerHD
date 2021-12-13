7b8e7a5ac9ed5027f7c060766f088c132b5689bf
hamiltondowdy <92259496+hamiltondowdy@users.noreply.github.com>
2021-12-13 10:27:04 -0600

    add containers for schedule by hour

--- js/script.js
+++ js/script.js
@@ -1,13 +1,41 @@
-var currentDate = document.querySelector("#currentDay");
-var workDay = document.querySelector("#time-block");
+$(document).ready(function() {
+    $("currentDay").text(moment().format('ddd MMM Do YYYY'));
+})
 
-currentDate = moment();
-displayCurrentDate(currentDate);
+//function that sets the background color based on current hour
 
+//define each row as an hour (24hr format)
+const rows = document.getElementById("row");
+let currentHour = parseInt(moment().format('H'));
 
-function displayCurrentDate(currentDate) {
-    document.getElementById("currentDay")
-    .textContent=currentDate.format("MM/DD/YYYY");
+console.log(currentHour);
+
+Array.from(rows).forEach(row => {
+    let
+      rowIdString = row.id,
+      rowHour;
+    if (rowIdString) {
+      rowHour = parseInt(rowIdString);
+    }
+    if (rowHour) {
+      // Compares row id to current hour and sets color accordingly
+      if (currentHour === rowHour) {
+        setColor(row, "red");
+      } else if ((currentHour < rowHour)){
+        setColor(row, "green");
+      } else {
+        setColor(row, "lightgrey");
+      }
+    }
+  });
+
+function setColor(element, color) {
+    element.style.backgroundColor = color;
 }
 
+function saveData() {
+    var input = document.getElementById("textarea").textContent;
+    localStorage.setItem("textarea", input);
+}
+document.getElementById("textarea").textContent = localStorage.getItem("textarea");
 
